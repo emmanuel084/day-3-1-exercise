caca2eebabc29fef7c827cc5700bca8fdbd5ffe8
Emmanuel Castillo <>
2021-05-08 01:19:25 +0000

    Finished.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ number = int(input("Which number do you want to check? "))
 #Write your code below this line 👇
 number_in = number % 2 
 if number_in == 0:
-  print("This is an even number")
+  print("This is an even number.")
 else:
-  print("This is an odd number")
+  print("This is an odd number.")
 
 # print(number_in)
